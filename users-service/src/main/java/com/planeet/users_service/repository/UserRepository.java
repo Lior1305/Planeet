31d2c7777a8ab9be9d49192e21acf39b6bb724ea
Lior1305 <lior.berlin@gmail.com>
2025-08-21 17:16:48 +0300

    some changes

--- users-service/src/main/java/com/planeet/users_service/repository/UserRepository.java
+++ users-service/src/main/java/com/planeet/users_service/repository/UserRepository.java
@@ -4,7 +4,6 @@ import com.planeet.users_service.model.User;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Mono;
 
-import java.util.Map;
 
 public interface UserRepository extends ReactiveMongoRepository<User, String> {
     Mono<User> findByUsername(String username);
